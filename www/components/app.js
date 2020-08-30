c741ee805d08fd2804feca0ab514ec1f2451e4f1
Jorge-Edson <60950040+Jorge-Edson@users.noreply.github.com>
2020-08-31 05:21:30 +0900

    Aperfeiçoamento do funcionamento do APP e aprimoramento do layout.

--- www/components/app.js
+++ www/components/app.js
@@ -1,30 +1,27 @@
 $(document).on("click", "#Calcular", function()
 {
+  //Declaração de variáveis.
   var atividade = $("#Atividade").val();
   var idade = $("#Idade").val();
   var altura = $("#Altura").val();
   var peso = $("#Peso").val();
   var sexo = $("#Sexo").val();
   var resultado;
-
-  switch (atividade)
-  {
-    case 1: atividade = null;
-    case 2: atividade = 1.2;
-    case 3: atividade = 1.375;
-    case 4: atividade = 1.55;
-    case 5: atividade = 1.725;
-    case 6: atividade = 1.9;
-  }  
+  var resultado1;
   
+  //Cálculo de gasto basal e taxa de metabolismo basal.
   if (sexo == 2)
   {
-    resultado = atividade * (66 + ((13.7 * peso) + (5 * altura) - (6.8 * idade)));
+    resultado = Math.round(66 + ((13.7 * peso) + (5 * altura) - (6.8 * idade)));
+    resultado1 = Math.round(atividade * resultado);
   }
   else if (sexo == 3)
   {
-    resultado = atividade * (655 + ((9.6 * peso) + (1.8 * altura) - (4.7 * idade)));
+    resultado = Math.round(655 + ((9.6 * peso) + (1.8 * altura) - (4.7 * idade)));
+    resultado1 = Math.round(atividade * resultado);
   }
-
+  
+  //Exibição de resultados.
   $("#Resultado").val(resultado);
+  $("#MetabolismoBasal").val(resultado1);
 });
